0ef91bde1c4304cae207f243eb7e9025af080f85
Cristobal Dominguez <cristobald@gmail.com>
2018-08-29 17:12:50 -0300

    Agrega animación para acceder a los servicios

--- wp-content/themes/Tema_Kineduomed/assets/js/functions.js
+++ wp-content/themes/Tema_Kineduomed/assets/js/functions.js
@@ -1,5 +1,32 @@
-jQuery(document).ready( function($) {
+function getUrlVars() {
+	var vars = {}
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value
+	})
+	return vars
+}
 
-	// Functions
+function folder(position) {
+	var dir = window.location.pathname.split('/')
+	return dir[position]
+}
 
-});
+function scrollTo(aid){
+	var aTag = $(aid)
+	$('html,body').animate({
+		scrollTop: aTag.offset().top -180
+	},'slow')
+}
+
+
+(function($) {
+
+	if ( folder(1) === 'servicios' ) {
+		var ver = getUrlVars()["goto"]
+
+		if (ver !== null) {
+			scrollTo( $('#' + ver) )
+		}
+	}
+
+})(window.jQuery)
